bbac662dfbe78033ab0f660e94b203ad7837f066
jim29-icu <jim29sozasancho@gmail.com>
2025-07-24 13:13:57 -0600

    mod conexion

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-MONGO_URI = "mongodb+srv://jimmysoza:8VQfT5HLgDfXc4cQ@cluster0.azkuc1h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_URI = "mongodb+srv://jimmysoza:8VQfT5HLgDfXc4cQ@cluster0.azkuc1h.mongodb.net/?retryWrites=true&w=majority&tls=true&appName=Cluster0"
 
