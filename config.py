158daa7c991f435bb2ec84fe62b502f48c89e924
jim29-icu <jim29sozasancho@gmail.com>
2025-07-24 13:33:57 -0600

    upd conex local
    Deploy local and server

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Carga variables desde .env
 
 MONGO_URI = os.getenv("MONGO_URI")
 
-if MONGO_URI is None:
+if not MONGO_URI:
     raise Exception("Error: La variable de entorno MONGO_URI no está definida.")
 
 
